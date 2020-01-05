2649f1c7d29227ed73c437357cb4dec74ed994dd
Kacper Mazur <kacper.mazur888@gmail.com>
2020-01-05 23:58:54 +0000

    message router

---
+++ server/routes/messages.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+let Message = require("../models/message.model");
+
+router.route("/").get((req, res) => {
+  Message.find({}).exec((err, message) => {
+    if (err) {
+      res.send("ROUTER: " + err).status(500);
+    } else {
+      res.send(message).status(200);
+    }
+  });
+});
+
+router.route("/send").post((req, res) => {
+    Message.create(req.body)
+    .then(message => {
+        res.send(message).status(200);
+    })
+    .catch(err => {
+        console.log("ROUTER: " + err).status(500);
+    });
+}});
