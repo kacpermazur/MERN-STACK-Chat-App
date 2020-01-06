fcfbbeac8fbbbc93b60b04f9557a24f1ac136576
Kacper Mazur <kacper.mazur888@gmail.com>
2020-01-06 04:27:20 +0000

    message looping

--- client/src/components/chat-element.component.js
+++ client/src/components/chat-element.component.js
@@ -5,17 +5,23 @@ class ChatElement extends Component {
   render() {
     return (
       <Comment.Group>
-        <Comment.Avatar
-          style={{
-            height: "50px",
-            width: "50px",
-            float: "left",
-            marginRight: "1em"
-          }}
-          src={"https://image.flaticon.com/icons/svg/149/149071.svg"}
-        />
-        <Comment.Author as="a">Matt</Comment.Author>
-        <Comment.Text>How artistic!</Comment.Text>
+        {this.props.messages.map((message, index) => {
+          return (
+            <Comment key={"c" + index}>
+              <Comment.Avatar
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  float: "left",
+                  marginRight: "1em"
+                }}
+                src={"https://image.flaticon.com/icons/svg/149/149071.svg"}
+              />
+              <Comment.Author as="a">{message.username}}</Comment.Author>
+              <Comment.Text>{message.content}</Comment.Text>
+            </Comment>
+          );
+        })}
       </Comment.Group>
     );
   }
